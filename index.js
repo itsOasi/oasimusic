e6058ffea3f54ae0f7eb23be1929b2401534ee8d
itsOasi <ostally001@gmail.com>
2024-11-10 17:35:13 -0700

    UI updates, added procedurally generated links and animated header

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 let get_latest = (data) => {
 	let hero = document.getElementById("hero")
 	let title = document.getElementById("song-title")
-	let vid = sortPopular(data, "latest")
+	let vid = sortLatest(data, "latest")
+	let download_url = get_download_url(vid.item.description);
+	let watch_url = `https://www.youtube.com/watch?v=${vid.key}`;
 	console.log(vid)
-	title.innerHTML = vid.title;
-	if (vid.thumbnails.maxres)
-		hero.style.backgroundImage = `url(${vid.thumbnails.maxres.url})`
+	title.innerHTML = vid.item.title;
+	if (vid.item.thumbnails.maxres)
+		hero.style.backgroundImage = `url(${vid.item.thumbnails.maxres.url})`
 	else
-		hero.style.backgroundImage = `url(${vid.thumbnails.high.url})`
+		hero.style.backgroundImage = `url(${vid.item.thumbnails.high.url})`
 	hero.style.backgroundSize = "cover"
 	hero.style.backgroundPosition = "center"
+	document.getElementById("download-latest").href = download_url
+	document.getElementById("watch-latest").href = watch_url
 	
 }
 
 
 function get_popular(data) {
-	sortLatest(data, "releases")
-}
-
-function sortPopular(data, el_id) {
-	data_el = document.getElementById(el_id)
-	console.log(data)
-	videos = []
-	for (item in data) {
-		videos.push(data[item])
-	}
-	// videos.sort((a, b)=> b.publishedAt - a.publishedAt)
-	videos.sort((a, b)=> b.viewCount - a.viewCount)
-	return videos[0]
-
+	sortPopular(data, "releases")
 }
 
 function sortLatest(data, el_id) {
@@ -36,23 +27,59 @@ function sortLatest(data, el_id) {
 	console.log(data)
 	videos = []
 	for (item in data) {
-		videos.push(data[item])
+		videos.push({"item":data[item], "key":item})
 	}
-	videos.sort((a, b)=> b.viewCount - a.viewCount)
+	// videos.sort((a, b)=> b.publishedAt - a.publishedAt)
+	videos.sort((a, b)=> b.item.viewCount - a.item.viewCount)
+	return videos[0]
+
+}
+
+function sortPopular(data, el_id) {
+	data_el = document.getElementById(el_id)
+	console.log(data)
+	videos = []
+	for (item in data) {
+		videos.push({"item":data[item], "key":item})
+	}
+	videos.sort((a, b)=> b.item.viewCount - a.item.viewCount)
 	let count = 0;
 	videos.forEach(v => {
 		if (count < 3) {
-			data_el.innerHTML += `<div class="song"><img src="${v.thumbnails.default.url}"><div class="song-details"><p>${v.title}</p><p>${v.viewCount}</p></div></div>`
+			data_el.innerHTML += `<div class="song"><img src="${v.item.thumbnails.default.url}"><div class="song-details"><h1>${v.item.title}</h1><a href="${get_download_url(v.item.description)}" class="btn btn-lg btn-primary">Download Now > </a><a href="https://www.youtube.com/watch?v=${v.key}" class="btn btn-secondary">Watch Now</a></div></div>`
 			count++
 		}
 	});
 }
 
+function get_download_url(desc){
+	let url = ""
+	let lines = desc.split("\n")
+	for (line in lines) {
+		if (lines[line].includes("Download: ")) {
+			url = lines[line].split(" ")[1]
+		}
+	}
+	console.log(url)
+	return url
+}
+
 window.onload = () => {
-	fetch("http://localhost:8000/get_yt_data")
+	document.querySelector("nav").style.backgroundColor = `rgba(30, 30, 30, ${(window.scrollY / 100)})`
+	fetch("http://192.168.0.234:8000/get_yt_data")
 		.then((response) => response.json())
 		.then((data) => {
 			get_latest(data)
 			get_popular(data)
 		})
+}
+
+window.onscroll = () => {
+	if (window.scrollY < 100) {
+		console.log("scrolling")
+		document.querySelector("nav").style.backgroundColor = `rgba(30, 30, 30, ${(window.scrollY / 100)})`
+	} else {
+		document.querySelector("nav").style.opacity = "1"
+		document.querySelector("nav").style.backgroundColor = "var(--background)"
+	}
 }
